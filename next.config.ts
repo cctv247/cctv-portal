503fb1a74a01d021e25ce95b08c961d0c55877f0
wazahul <me.wazahul@gmail.com>
2026-04-15 22:05:06 +0530

    Full Refresh: Fixed TS errors and synced UI

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Agar images ya external data hai toh domains allow karein
   images: {
-    domains: ['localhost', '192.168.0.108'], 
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000', // Agar aapka local server kisi specific port par hai
+      },
+      {
+        protocol: 'http',
+        hostname: '192.168.0.108',
+        pathname: '/**', // Isse is IP ke saare images allow ho jayenge
+      },
+    ],
   },
 };
+
 export default nextConfig;
